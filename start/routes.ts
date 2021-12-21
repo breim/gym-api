af7e6676e11745692869bc94dde4ad7fb5766afb
Henrique Breim <henrique@breim.com.br>
2021-12-21 09:01:45 -0300

    feat: add routes and midleware auth

--- start/routes.ts
+++ start/routes.ts
@@ -1,5 +1,13 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async () => {
-  return { hello: 'world' }
+  return { hello: 'nothing to see here 🙈' }
 })
+
+Route.group(() => {
+  Route.post('sign_up', 'AuthController.sign_up')
+  Route.post('sign_in', 'AuthController.sign_in')
+  Route.group(() => {
+    Route.get('users/me', 'UsersController.me')
+  }).middleware('auth:api')
+}).prefix('api')
