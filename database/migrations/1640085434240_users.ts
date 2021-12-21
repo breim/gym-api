cdfa27bb4a738401fca4d5de62c6283402a278f4
Breim <henrique@breim.com.br>
2021-12-21 08:20:09 -0300

    feat: configure db and generate users model

---
+++ database/migrations/1640085434240_users.ts
@@ -0,0 +1,21 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class UsersSchema extends BaseSchema {
+  protected tableName = 'users'
+  public static selfAssignPrimaryKey = true
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary().defaultTo(this.db.rawQuery('uuid_generate_v4()').knexQuery)
+      table.string('name', 255)
+      table.string('email', 255).unique().notNullable()
+      table.string('password', 180).notNullable()
+      table.string('remember_me_token').nullable()
+      table.timestamps(true)
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
